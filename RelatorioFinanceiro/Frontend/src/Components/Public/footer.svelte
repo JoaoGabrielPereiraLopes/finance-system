6198fa9dcaf0569c88a19c4e87605add58a028f6
JoaoGabrielPereiraLopes <gabriel.pitito123@gmail.com>
2026-06-03 17:26:53 -0300

    .gitignore

---
+++ RelatorioFinanceiro/Frontend/src/Components/Public/footer.svelte
@@ -0,0 +1,8 @@
+<footer class="md:hidden flex w-full bg-blue-600 py-5">
+    <div class="flex flex-row text-white md:text-2xl lg:md:gap-30 text-4xl text-white w-full justify-around">
+        <i class="fa-solid fa-house hover:cursor-pointer hover:text-gray-200"></i>
+        <i class="fa-solid fa-money-bill-trend-up hover:cursor-pointer hover:text-gray-200"></i>
+        <i class="fa-solid fa-money-bill hover:cursor-pointer hover:text-gray-200"></i>
+        <i class="fa-solid fa-credit-card hover:cursor-pointer hover:text-gray-200"></i>
+    </div>
+</footer>
---
+++ RelatorioFinanceiro/Frontend/src/Components/Public/nav.svelte
@@ -0,0 +1,19 @@
+<nav class="w-full bg-blue-600 py-5 px-10 md:px-20 text-center flex items-center text-white justify-between">
+    
+    <div class="flex flex-row items-center gap-5">
+        <i class="fa-solid fa-coins text-4xl"aria-hidden="true"></i>
+        <p class="hidden md:text-xl md:block" style="font-family: Inter;">FINANCE</p>
+    </div>
+    <div class="hidden md:flex flex-row px-15 gap-5 text-white lg:gap-30 md:text-4xl">
+        <i class="fa-solid fa-house hover:cursor-pointer hover:text-gray-200"></i>
+        <i class="fa-solid fa-money-bill-trend-up hover:cursor-pointer hover:text-gray-200"></i>
+        <i class="fa-solid fa-money-bill hover:cursor-pointer hover:text-gray-200"></i>
+        <i class="fa-solid fa-credit-card hover:cursor-pointer hover:text-gray-200"></i>
+    </div>
+
+    <div class="flex flex-row items-center  gap-5 text-white text-4xl">
+        <i class="fa-solid fa-circle-user"></i>
+        <p>User</p>
+    </div>
+    
+</nav>
